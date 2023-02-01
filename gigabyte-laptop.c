0d70acab9d8894bbc18a3c9c407902d674c50917
Albert Tang <tangalbert919@yahoo.com>
2023-02-01 09:23:58 -0600

    Add WMBD method, improve documentation
    Move WMI methods to the top as well.

--- gigabyte-laptop.c
+++ gigabyte-laptop.c
@@ -30,6 +30,7 @@ MODULE_VERSION(GIGABYTE_LAPTOP_VERSION);
 #define WMI_METHOD_WMBC "ABBC0F6F-8EA1-11D1-00A0-C90629100000" // Seems to only return values
 #define WMI_METHOD_WMBD "ABBC0F75-8EA1-11D1-00A0-C90629100000" // Will probably do most of the work.
 #define WMI_WMBC_METHOD "\\_SB.PCI0.AMW0.WMBC"
+#define WMI_WMBD_METHOD "\\_SB.PCI0.AMW0.WMBD"
 
 /* _SB_.WFDE._WDG */
 #define WMI_EVENT_WFDE "A6FEA33E-DABF-46F5-BFC8-460D961BEC9F"
@@ -52,7 +53,85 @@ struct gigabyte_laptop_wmi {
 
 static struct platform_device *platform_device;
 
-/* hwmon */
+/* WMI methods ********************************************/
+
+/* WMBC method (checks value in EC) */
+static int gigabyte_laptop_get_devstate(u32 arg1, int *result)
+{
+	union acpi_object args[3], *obj;
+	acpi_status status;
+	acpi_handle handle;
+	struct acpi_object_list params;
+	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
+
+	status = acpi_get_handle(NULL, (acpi_string) WMI_WMBC_METHOD, &handle);
+	if (ACPI_FAILURE(status)) {
+		pr_err("Cannot get handle\n");
+		return -1;
+	}
+
+	args[0].type = ACPI_TYPE_INTEGER;
+	args[0].integer.value = 0;
+	args[1].type = ACPI_TYPE_INTEGER;
+	args[1].integer.value = arg1;
+	args[2].type = ACPI_TYPE_INTEGER;
+	args[2].integer.value = 0;
+	params.count = 3;
+	params.pointer = args;
+
+	status = acpi_evaluate_object(handle, NULL, &params, &buffer);
+	if (ACPI_FAILURE(status))
+		return -1;
+
+	obj = buffer.pointer;
+	if (obj && obj->type == ACPI_TYPE_INTEGER)
+		*result = obj->integer.value;
+	else {
+		kfree(obj);
+		return -EINVAL;
+	}
+	return 0;
+}
+
+/* WMBD method (sets value in EC) */
+static int gigabyte_laptop_set_devstate(u32 arg1, u32 arg2, int *result)
+{
+	union acpi_object args[3], *obj;
+	acpi_status status;
+	acpi_handle handle;
+	struct acpi_object_list params;
+	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
+
+	status = acpi_get_handle(NULL, (acpi_string) WMI_WMBD_METHOD, &handle);
+	if (ACPI_FAILURE(status)) {
+		pr_err("Cannot get handle\n");
+		return -1;
+	}
+
+	args[0].type = ACPI_TYPE_INTEGER;
+	args[0].integer.value = 0;
+	args[1].type = ACPI_TYPE_INTEGER;
+	args[1].integer.value = arg1;
+	args[2].type = ACPI_TYPE_INTEGER;
+	args[2].integer.value = arg2;
+	params.count = 3;
+	params.pointer = args;
+
+	status = acpi_evaluate_object(handle, NULL, &params, &buffer);
+	if (ACPI_FAILURE(status))
+		return -1;
+
+	obj = buffer.pointer;
+	if (obj && obj->type == ACPI_TYPE_INTEGER)
+		*result = obj->integer.value;
+	else {
+		kfree(obj);
+		return -EINVAL;
+	}
+	return 0;
+}
+
+/* hwmon **************************************************/
 static umode_t gigabyte_laptop_hwmon_is_visible(const void *data, enum hwmon_sensor_types type,
 					u32 attr, int channel)
 {
@@ -95,10 +174,10 @@ static const struct hwmon_chip_info gigabyte_laptop_chip_info = {
 	.info = gigabyte_laptop_hwmon_info,
 };
 
-/* sysfs methods */
+/* sysfs **************************************************/
 
 /*
- * Show fan mode.
+ * Fan mode.
  * 0 = normal fan mode
  * 1 = silent fan mode
  * 2 = gaming fan mode
@@ -145,44 +224,6 @@ static const struct attribute_group gigabyte_laptop_attr_group = {
 	//.is_visible = gigabyte_laptop_sysfs_is_visible,
 	.attrs = gigabyte_laptop_attributes,
 };
-
-/* The WMBC method is used here. */
-static int gigabyte_laptop_get_devstate(u32 arg1, struct acpi_buffer *output, int *result)
-{
-	union acpi_object args[3], *obj;
-	acpi_status status;
-	acpi_handle handle;
-	struct acpi_object_list params;
-	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
-
-	status = acpi_get_handle(NULL, (acpi_string) WMI_WMBC_METHOD, &handle);
-	if (ACPI_FAILURE(status)) {
-		pr_err("Cannot get handle\n");
-		return -1;
-	}
-
-	args[0].type = ACPI_TYPE_INTEGER;
-	args[0].integer.value = 0;
-	args[1].type = ACPI_TYPE_INTEGER;
-	args[1].integer.value = arg1;
-	args[2].type = ACPI_TYPE_INTEGER;
-	args[2].integer.value = 0;
-	params.count = 3;
-	params.pointer = args;
-
-	status = acpi_evaluate_object(handle, NULL, &params, &buffer);
-	if (ACPI_FAILURE(status))
-		return -1;
-
-	obj = buffer.pointer;
-	if (obj && obj->type == ACPI_TYPE_INTEGER)
-		*result = obj->integer.value;
-	else {
-		kfree(obj);
-		return -EINVAL;
-	}
-	return 0;
-}
 
 static struct platform_driver platform_driver = {
 	.driver = {
